6ce6ea83e80f9cefa01e0413baa65b2cff9e3ef0
AM1609 <anhminh16092002@gmail.com>
2024-05-07 15:46:18 +0700

    Update apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,14 +31,19 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    dependenciesInfo {
+        includeInApk = true
+        includeInBundle = true
+    }
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
-    implementation("com.google.firebase:firebase-firestore:24.10.3")
-    implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
     val nav_version = "2.7.7"
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -46,8 +51,9 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment:$nav_version")
     implementation("androidx.navigation:navigation-ui:$nav_version")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("com.firebaseui:firebase-ui-storage:7.2.0")
+    implementation("com.firebaseui:firebase-ui-storage:8.0.2")
     implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
 
 
 
